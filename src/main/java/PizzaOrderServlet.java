7f74994620c2d7fdf4262803f753a5fa1053df28
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-21 12:33:57 -0600

    remove commented out code

--- src/main/java/PizzaOrderServlet.java
+++ src/main/java/PizzaOrderServlet.java
@@ -1,5 +1,4 @@
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
