ae59ef107f2a87fd49a267f90863dda6669c8e88
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-25 14:31:39 -0600

    add: sessions and cookies exercise

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -1,2 +1,23 @@
-public class LogoutServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    //TODO: When a user visits /logout, they should be logged out of your application and redirected to the login page.
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        if(session != null) {
+            session.removeAttribute("user");
+            session.invalidate();
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -9,7 +9,8 @@
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Viewing your profile.</h1>
+        <h1>Welcome, ${user}!</h1>
+        <h1>You are now viewing your profile.</h1>
     </div>
 
 </body>
