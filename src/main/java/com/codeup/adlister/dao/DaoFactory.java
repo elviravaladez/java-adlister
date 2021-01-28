66873903cf2d6fd7f69170a7c613d31cab9f1e09
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-28 15:21:32 -0600

    update: DaoFactory

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -3,11 +3,20 @@ package com.codeup.adlister.dao;
 public class DaoFactory {
     private static Ads adsDao;
     private static Config config = new Config();
+    private static Users usersDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+
+    //TODO: Add functionality to the DaoFactory class to return an instance of the users data access object.
+    public static Users getUsersDao() {
+        if(usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
     }
 }
